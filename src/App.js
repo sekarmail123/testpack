0089e28ee24f9e91da364af23ad3e0c960847329
sekarmail123 <sekarmail123@gmail.com>
2023-07-12 18:22:18 +0530

    test setup
    test setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2> sdfsdfStart editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
